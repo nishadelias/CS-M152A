59e121cad9c343868c0843d6584af45d7d1b8fb8
nishadelias <123427128+nishadelias@users.noreply.github.com>
2024-05-02 10:36:34 -0700

    Update tb.v

--- tb.v
+++ tb.v
@@ -1,3 +1,5 @@
+// Code your testbench here
+// or browse Examples
 `timescale 1ns / 1ps
 
 module tb;
@@ -27,22 +29,37 @@ module tb;
         // Test 1: Positive number
         D = 12'b010110011001; // Random positive number
         #10; // Wait 10ns for changes to be reflected in outputs
-        
+      
+      $display("Test 1: %b", {S, E, F});
+        #10
+      
         // Test 2: Negative number
         D = 12'b101001110010; // Random negative number
         #10;
+      
+      $display("Test 2: %b", {S, E, F});
+        #10
         
         // Test 3: Smallest negative number
         D = 12'b100000000000; // -2048 in two's complement
         #10;
+      
+      $display("Test 3: %b", {S, E, F});
+        #10
         
         // Test 4: Positive number with rounding
         D = 12'b010101011111;
         #10;
+      
+      $display("Test 4: %b", {S, E, F});
+        #10
         
         // Test 5: Corner case of rounding
         D = 12'b001111100000;
         #10;
+      
+      $display("Test 5: %b", {S, E, F});
+        #10
         
         // Add more tests if required...
         
